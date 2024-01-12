91bab28fb0fe6c15bdf0f23bfe44a35f1e46fb7e
Nitor112221 <developer@yandex.ru>
2024-01-12 11:43:11 +0500

    теперь враги не умеют видеть через стены

--- scripts/entity/Enemy.py
+++ scripts/entity/Enemy.py
@@ -4,14 +4,18 @@ from scripts.entity.Entity import Entity
 import scripts.tools as tools
 
 
-def line_rect_intersection(x1, y1, x2, y2, rect):
-    return True
+def intersection(ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int):
+    v1 = (bx2 - bx1) * (ay1 - by1) - (by2 - by1) * (ax1 - bx1)
+    v2 = (bx2 - bx1) * (ay2 - by1) - (by2 - by1) * (ax2 - bx1)
+    v3 = (ax2 - ax1) * (by1 - ay1) - (ay2 - ay1) * (bx1 - ax1)
+    v4 = (ax2 - ax1) * (by2 - ay1) - (ay2 - ay1) * (bx2 - ax1)
+    return (v1 * v2 < 0) and (v3 * v4 < 0)
 
 
 class Enemy(Entity):
     def __init__(self, pos_x: int, pos_y: int, animation: dict, image: pygame.Surface, *group):
         super().__init__(image, pos_x, pos_y, animation, *group)
-        self.sight_distance = 8 * 10
+        self.sight_distance = 8 * 15
         self.irascibilis = False
 
     def update(self, player, tile_group):
@@ -19,7 +23,14 @@ class Enemy(Entity):
         if ((abs(self.rect.centerx - player.rect.x) ** 2) + (
                 abs(self.rect.centery - player.rect.y) ** 2)) ** 0.5 <= self.sight_distance:
             for sprite in tile_group:
-                if not line_rect_intersection(*self.rect.center, *player.rect.center, sprite.rect):
+                if intersection(self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery,
+                                *sprite.rect.topleft, *sprite.rect.bottomleft) or \
+                        intersection(self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery,
+                                     *sprite.rect.topleft, *sprite.rect.topright) or \
+                        intersection(self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery,
+                                     *sprite.rect.bottomright, *sprite.rect.bottomleft) or \
+                        intersection(self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery,
+                                     *sprite.rect.topright, *sprite.rect.bottomright):
                     self.irascibilis = False
                     return
             self.irascibilis = True
